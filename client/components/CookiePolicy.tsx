45304835cae80229dc420233eac9cab99fc46809
Peter Klingelhofer <peterklingelhofer@gmail.com>
2022-02-20 10:53:54 -0600

    add directions for iOS to allow Cookie Policy

--- client/components/CookiePolicy.tsx
+++ client/components/CookiePolicy.tsx
@@ -1,17 +1,41 @@
 import Constants from 'expo-constants';
 import React from 'react';
-import { Image, Linking, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, Linking, Platform, ScrollView, StyleSheet, Text } from 'react-native';
 
-export default function TrackingPermissionsDeclined() {
+export default function CookiePolicy() {
   return (
     <ScrollView style={styles.container}>
       <Image
         style={styles.image}
         source={require('../../assets/icon.png')}
       />
+      {Platform.OS === 'ios' && (
+        <>
+          <Text
+            style={styles.text}>
+            If you are not prompted requesting permissions, go to
+          </Text>
+          <Text
+            style={{ ...styles.boldText, ...styles.text }}>
+            Settings &gt; Privacy &gt; Tracking
+          </Text>
+          <Text
+            style={styles.text}>
+            and enable
+          </Text>
+          <Text
+            style={{ ...styles.boldText, ...styles.text }}>
+            Allow Apps to Request to Track
+            {"\n"}
+          </Text>
+        </>
+      )}
       <Text
         style={styles.text}>
-        To ensure you get the best experience, Resonate uses cookies. Functional cookies are used to keep you logged in for a while and remember your theme settings. Disallowing tracking permissions precludes the use of this app.{"\n"}
+        {"\n"}
+        To ensure you get the best experience, Resonate uses cookies.
+        Functional cookies are used to keep you logged in for a while and remember your theme settings.
+        Disallowing tracking permissions precludes the use of this app.
         {"\n"}
       </Text>
       <Text
@@ -24,10 +48,13 @@ export default function TrackingPermissionsDeclined() {
 }
 
 const styles = StyleSheet.create({
+  boldText: {
+    fontWeight: 'bold',
+  },
   container: {
     flex: 1,
     marginTop: Constants.statusBarHeight,
-    marginHorizontal: Constants.statusBarHeight * 1.5,
+    marginHorizontal: '10%',
   },
   link: {
     fontSize: 24,
@@ -35,11 +62,10 @@ const styles = StyleSheet.create({
   },
   image: {
     height: 360,
-    marginLeft: '10%',
-    marginRight: '10%',
+    marginHorizontal: '10%',
     width: '80%',
   },
   text: {
     fontSize: 20,
-  }
+  },
 });
