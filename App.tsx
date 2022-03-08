dec9cd243905ef8170135785d099a15626bd7a31
Peter Klingelhofer <peterklingelhofer@gmail.com>
2022-03-08 17:19:00 -0600

    fix tab spacing

--- App.tsx
+++ App.tsx
@@ -1,90 +1,90 @@
 import * as SplashScreen from 'expo-splash-screen';
 import {
-	getTrackingPermissionsAsync,
-	requestTrackingPermissionsAsync,
+    getTrackingPermissionsAsync,
+    requestTrackingPermissionsAsync,
 } from 'expo-tracking-transparency';
 import React, {
-	useEffect,
-	useState,
+    useEffect,
+    useState,
 } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { WebView } from 'react-native-webview';
 import CustomStatusBar from './client/components/CustomStatusBar';
 import {
-	cookiePreferences,
-	getPermissionsGranted,
-	storePermissionsGranted,
+    cookiePreferences,
+    getPermissionsGranted,
+    storePermissionsGranted,
 } from './client/helpers/permissions';
 import { originAllowList } from './client/services/originAllowList';
 
 export default function App() {
-	const [areTrackingPermissionsGranted, setAreTrackingPermissionsGranted] = useState(false);
-	const [didPermissionsChange, setDidPermissionsChange] = useState(false);
-	const [isRequestingPermissions, setIsRequestingPermissions] = useState(true);
-	const [webViewKey, setWebViewKey] = useState(0);
+    const [areTrackingPermissionsGranted, setAreTrackingPermissionsGranted] = useState(false);
+    const [didPermissionsChange, setDidPermissionsChange] = useState(false);
+    const [isRequestingPermissions, setIsRequestingPermissions] = useState(true);
+    const [webViewKey, setWebViewKey] = useState(0);
 
-	const hideSplashScreen = async () => await SplashScreen.hideAsync();
-	const reloadWebView = () => setWebViewKey(webViewKey + 1)
+    const hideSplashScreen = async () => await SplashScreen.hideAsync();
+    const reloadWebView = () => setWebViewKey(webViewKey + 1)
 
-	useEffect(() => {
-		(async () => {
-			await SplashScreen.preventAutoHideAsync();
-			const arePermissionsGrantedHistorically = await getPermissionsGranted();
-			const { granted } = await getTrackingPermissionsAsync();
-			if (granted) {
-				setAreTrackingPermissionsGranted(true);
-				if (!arePermissionsGrantedHistorically) {
-					storePermissionsGranted(true);
-					setDidPermissionsChange(true);
-				}
-			} else {
-				const { status } = await requestTrackingPermissionsAsync();
-				const arePermissionsGranted = status === 'granted';
-				setAreTrackingPermissionsGranted(arePermissionsGranted);
-				if (arePermissionsGranted !== arePermissionsGrantedHistorically) {
-					storePermissionsGranted(arePermissionsGranted);
-					setDidPermissionsChange(true);
-				}
-			}
+    useEffect(() => {
+        (async () => {
+            await SplashScreen.preventAutoHideAsync();
+            const arePermissionsGrantedHistorically = await getPermissionsGranted();
+            const { granted } = await getTrackingPermissionsAsync();
+            if (granted) {
+                setAreTrackingPermissionsGranted(true);
+                if (!arePermissionsGrantedHistorically) {
+                    setDidPermissionsChange(true);
+                    storePermissionsGranted(true);
+                }
+            } else {
+                const { status } = await requestTrackingPermissionsAsync();
+                const arePermissionsGranted = status === 'granted';
+                setAreTrackingPermissionsGranted(arePermissionsGranted);
+                if (arePermissionsGranted !== arePermissionsGrantedHistorically) {
+                    setDidPermissionsChange(true);
+                    storePermissionsGranted(arePermissionsGranted);
+                }
+            }
 
-			setIsRequestingPermissions(false);
-		})();
-	}, []);
+            setIsRequestingPermissions(false);
+        })();
+    }, []);
 
-	return (
-		<SafeAreaProvider>
-			<CustomStatusBar />
-			{!isRequestingPermissions && <WebView
-				allowsBackForwardNavigationGestures
-				cacheEnabled={didPermissionsChange}
-				ignoreSilentHardwareSwitch
-				injectedJavaScript={cookiePreferences(areTrackingPermissionsGranted)}
-				javaScriptCanOpenWindowsAutomatically
-				key={webViewKey}
-				mediaPlaybackRequiresUserAction
-				onContentProcessDidTerminate={reloadWebView}
-				onLoadEnd={hideSplashScreen}
-				onRenderProcessGone={reloadWebView}
-				originWhitelist={originAllowList}
-				sharedCookiesEnabled={false}
-				source={{
-					uri: 'https://stream.resonate.coop/discover',
-					headers: {
-						'doNotTrack': areTrackingPermissionsGranted ? '0' : '1',
-					},
-				}}
-				startInLoadingState
-				style={styles.container}
-				textInteractionEnabled={false}
-			/>}
-		</SafeAreaProvider>
-	)
+    return (
+        <SafeAreaProvider>
+            <CustomStatusBar />
+            {!isRequestingPermissions && <WebView
+                allowsBackForwardNavigationGestures
+                cacheEnabled={didPermissionsChange}
+                ignoreSilentHardwareSwitch
+                injectedJavaScript={cookiePreferences(areTrackingPermissionsGranted)}
+                javaScriptCanOpenWindowsAutomatically
+                key={webViewKey}
+                mediaPlaybackRequiresUserAction
+                onContentProcessDidTerminate={reloadWebView}
+                onLoadEnd={hideSplashScreen}
+                onRenderProcessGone={reloadWebView}
+                originWhitelist={originAllowList}
+                sharedCookiesEnabled={false}
+                source={{
+                    headers: {
+                        'doNotTrack': areTrackingPermissionsGranted ? '0' : '1',
+                    },
+                    uri: 'https://stream.resonate.coop/discover',
+                }}
+                startInLoadingState
+                style={styles.container}
+                textInteractionEnabled={false}
+            />}
+        </SafeAreaProvider>
+    )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		marginTop: -2,
-	},
+    container: {
+        flex: 1,
+        marginTop: -2,
+    },
 });
