05d03ef6d3357351aa337a39bc02579423435591
Peter Klingelhofer <peterklingelhofer@gmail.com>
2022-03-07 19:06:26 -0600

    remove cookie disclaimer to improve load time

--- client/helpers/originAllowList.ts
+++ client/helpers/originAllowList.ts
@@ -1,11 +1,11 @@
 export const originAllowList: string[] = [
     'https://community.resonate.is',
+    'https://js.stripe.com',
+    'https://m.stripe.network',
+    'https://offen.resonate.is',
+    'https://offen.stream.resonate.coop',
     'https://resonate.is',
     'https://stream.resonate.coop',
     'https://stream.resonate.ninja',
-    'https://offen.resonate.is',
-    'https://offen.stream.resonate.coop',
     'https://stripe.com',
-    'https://js.stripe.com',
-    'https://m.stripe.network',
 ];
